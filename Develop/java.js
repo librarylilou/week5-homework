199219a1cc6660e434368b1649fe3dc43d8d8853
Valeria Rodriguez <val.lilou.rod@gmail.com>
2021-04-24 13:25:46 -0600

    file restructure

---
+++ Develop/java.js
@@ -0,0 +1,9 @@
+
+var DateTime = luxon.DateTime();
+   var localTime = DateTime.local().toLocaleString({ weekday: 'long', month: 'long', day: '2-digit' });
+
+   console.log(localTime.toString()); 
+   
+    
+   
+  
